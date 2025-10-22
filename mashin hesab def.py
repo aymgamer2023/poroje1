146119c4759ba3a42403d98bf65015db5b64a59b
aymgamer2023 <yashamansouri836@gmail.com>
2025-10-22 19:32:32 +0330

    Add files via upload

---
+++ mashin hesab def.py
@@ -0,0 +1,9 @@
+def mashin_hesab_def(num1, num2):
+    print(f"jamesh mishr: {num1 + num2}")
+    print(f"zarbesh mishe: {num1 * num2}")
+    print(f"menash mishe: {num1 - num2}")
+    if num2 != 0:
+        print(f"taghsimesh mishe: {num1 / num2}")
+    else:
+        print("nemituni bar 0 tagsim koni")
+mashin_hesab_def(10,3)
